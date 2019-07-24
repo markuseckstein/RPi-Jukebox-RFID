913d42ca915e66d288d3e225c517cd260fe85173
Markus Eckstein <markus@markuseckstein.de>
2019-07-24 22:40:31 +0200

    feat: Dockerize RPi-Jukebox

---
+++ container-webserver/htdocs/config.php
@@ -0,0 +1,6 @@
+<?php
+
+$conf               = array();
+$conf['base_url']   = ""; // usually => "", generally: end with trailing slash
+$conf['base_path']  = "/sharedvolume"; // absolute path to folder
+$conf['local_url']  = $_SERVER['SERVER_NAME']; // put the fixed IP or Local name here
